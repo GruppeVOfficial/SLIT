eea6f8c3b40f822e70c05adf9fdad70d8e993c88
Mathih13 <mathih13@uia.no>
2016-12-08 11:57:30 +0100

    GetAllStudents + Debug utility
    Added a getAllStudents() method in UserManager.
    
    Added Utils package with Debug class for misc. methods and easier
    access to functions such as “Debug.Print(string)” instead of
    system.out.println(“string”)

--- Slit-Client/src/java/GUI/Main.java
+++ Slit-Client/src/java/GUI/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("ModuleCreate.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("RegisterUser.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
