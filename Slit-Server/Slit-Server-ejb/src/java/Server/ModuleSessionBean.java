b79f71b27a056da45107c1168bee491924968167
erih14 <erik.haaland@hotmail.com>
2016-12-10 23:05:31 +0100

    fix problems queue

--- Slit-Server/Slit-Server-ejb/src/java/Server/ModuleSessionBean.java
+++ Slit-Server/Slit-Server-ejb/src/java/Server/ModuleSessionBean.java
@@ -186,5 +186,10 @@ public class ModuleSessionBean implements ModuleSessionBeanRemote {
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
 
+    @Override
+    public void removeModule(String Id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
 
 }
