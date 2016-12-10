48d2be2a66bf8c86cf195ddad9da9d154b8fc4bb
Brachii <eirsla@hotmail.com>
2016-12-10 16:03:25 +0100

    la til GUI for deliverable student

--- Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Deliverable_.java
+++ Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Deliverable_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-07T11:34:35")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-10T15:55:19")
 @StaticMetamodel(Deliverable.class)
 public class Deliverable_ { 
 
--- Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Queue_.java
+++ Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Queue_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-10T11:30:29")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-10T15:55:19")
 @StaticMetamodel(Queue.class)
 public class Queue_ { 
 
