91092d74afa2b075165c15db2337c8417088d02e
Brachii <eirsla@hotmail.com>
2016-12-10 16:48:49 +0100

    added login GUI

--- Slit-Client/src/java/GUI/Main.java
+++ Slit-Client/src/java/GUI/Main.java
@@ -28,7 +28,7 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("Queue.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("DeliverableStudent.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
--- Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Teacher_.java
+++ Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Teacher_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-10T15:55:19")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-10T16:23:09")
 @StaticMetamodel(Teacher.class)
 public class Teacher_ { 
 
