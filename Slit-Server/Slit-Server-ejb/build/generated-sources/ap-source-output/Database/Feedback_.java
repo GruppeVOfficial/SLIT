da5750a6fba7a30914bf50c19b7d0a1b704f83b5
Terje Heum Seljåsen <Terjeseljaasen@gmail.com>
2016-12-11 16:34:30 +0100

    what have i done??

--- Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Feedback_.java
+++ Slit-Server/Slit-Server-ejb/build/generated-sources/ap-source-output/Database/Feedback_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-10T17:10:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2016-12-11T16:21:59")
 @StaticMetamodel(Feedback.class)
 public class Feedback_ { 
 
